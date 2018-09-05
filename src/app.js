a07d28228929e25b0a88c21db614f263bb65800d
JortvD <jortvandriel@hotmail.com>
2018-09-05 20:27:10 +0200

    Added spacing to the app class

--- src/app.js
+++ src/app.js
@@ -7,12 +7,15 @@ module.exports = class Application {
 
 	async start() {
 		this.globalSettings = await this.globalSettingsManager.load();
+		
 		this.serverManager.start(this.globalSettings.server);
+
 		this.listening = true;
 	}
 
 	async stop() {
 		await this.globalSettingsManager.save(this.globalSettings);
+		
 		this.listening = false;
 	}
 }
