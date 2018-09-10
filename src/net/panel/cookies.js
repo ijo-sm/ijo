2fc4bf8b01496afb076c9174b2f3f900c608fd69
JortvD <jortvandriel@hotmail.com>
2018-09-10 18:20:08 +0200

    Added a cookie manager class for the response

---
+++ src/net/panel/cookies.js
@@ -0,0 +1,73 @@
+module.exports = class CookieManager {
+	constructor() {
+		this.map = new Map();
+	}
+
+	setCookie(name, value, options = {}) {
+		return this.map.set(name, new Cookie(name, value, options))
+	}
+
+	getCookie(name) {
+		return this.map.get(name);
+	}
+
+	hasCookie(name) {
+		return this.map.has(name);
+	}
+
+	removeCookie(name) {
+		return this.map.delete(name);
+	}
+
+	getCookies() {
+		return Array.from(this.map.values());
+	}
+
+	buildCookies() {
+		var cookies = [];
+
+		for(let cookie of this.map.values()) {
+			cookies.push(cookie.build());
+		}
+
+		return cookies;
+	}
+}
+
+class Cookie {
+	constructor(name, value, options) {
+		this.name = name;
+		this.value = value;
+		this.options = options;
+	}
+
+	build() {
+		var cookie = this.name + "=" + this.value;
+
+		if(this.options.expires instanceof Date) {
+			cookie += "; Expires=" + this.options.expires.toString();
+		}
+
+		if(typeof this.options.maxAge === "number") {
+			cookie += "; Max-Age=" + this.options.maxAge;
+		}
+
+		if(typeof this.options.domain === "string") {
+			cookie += "; Domain=" + this.options.domain;
+		}
+
+		if(typeof this.options.path === "string") {
+			cookie += "; Path=" + this.options.path;
+		}
+
+		if(this.options.secure) {
+			cookie += "; Secure";
+		}
+
+		if(this.options.httpOnly) {
+			cookie += "; HttpOnly";
+		}
+
+		return cookie;
+	}
+}
