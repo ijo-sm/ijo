e81fb608225e1edd6709854ac044d6c7ff435574
JortvD <jortvandriel@hotmail.com>
2018-09-22 21:57:51 +0200

    The test cleanup is done differently to speed it up

--- scripts/test.bat
+++ scripts/test.bat
@@ -26,16 +26,17 @@ call xcopy /s /y res\assets\src\pages res\assets\js >nul 2>&1
 call copy res\assets\src\jquery.min.js res\assets\js\jquery.min.js >nul 2>&1
 call uglifyjs-folder -y -o res\assets\js\panel.min.js -- res\assets\src\panel\ >nul 2>&1
 
-rem Cleanup and create the /test folder
-rmdir /s /q ".\test\" >nul 2>&1
-mkdir ".\test\"
+rem Cleanup (and if non-existant, create) the /test folder
+mkdir ".\test\" >nul 2>&1
+del ".\test\panel.json" >nul 2>&1
 
 rem Copy the ijo to /test/panel
 echo .git\ > exclude.txt
 echo \test\ >> exclude.txt
-xcopy ".\*" "..\test\panel\" /exclude:.\exclude.txt /d /h /y /e >nul 2>&1
-xcopy "..\test\panel\*.*" ".\test\panel\" /d /h /y /e >nul 2>&1
-rmdir /s /q "..\test\panel\" >nul 2>&1
+echo \node_modules\ >> exclude.txt
+xcopy ".\*" "..\.test-cache\panel\" /exclude:.\exclude.txt /d /h /y /e >nul 2>&1
+xcopy "..\.test-cache\panel\*" ".\test\panel\" /d /h /y /e >nul 2>&1
+xcopy ".\node_modules\*" ".\test\panel\node_modules\" /d /h /y /e >nul 2>&1
 del ".\exclude.txt" >nul 2>&1
 
 rem Ask user if machine needs to be installed
@@ -46,6 +47,7 @@ rem )
 
 pushd ".\test\panel\"
 rem Create the /data folder
+rmdir /s /q ".\data" >nul 2>&1
 mkdir ".\data"
 
 rem Install the ijo command
