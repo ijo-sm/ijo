ae81f7e2b57bebffca8178aeb1106032d048af75
JortvD <jortvandriel@hotmail.com>
2018-09-23 15:06:44 +0200

    Started on a plugin manager

--- src/plugin/manager.js
+++ src/plugin/manager.js
@@ -1,3 +1,15 @@
 const FileSystem = require("fs");
 const Path = require("path");
 const pify = require("pify");
+
+module.exports = class PluginManager {
+	constructor() {
+		this.plugins = {};
+	}
+
+	async load() {
+		var files = await pify(FileSystem.readdir)(Path.resolve(__dirname, "../../plugins/"));
+
+		console.log(files);
+	}
+}
