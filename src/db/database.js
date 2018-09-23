653d127df4e116e90b6ba096dd14067f954a0469
JortvD <jortvandriel@hotmail.com>
2018-09-23 15:05:30 +0200

    Redid the database class

---
+++ src/db/database.js
@@ -0,0 +1,57 @@
+const low = require("lowdb");
+const FileAsync = require("lowdb/adapters/FileAsync");
+const Path = require("path");
+
+function loadDatabase(file) {
+	return low(new FileAsync(file));
+}
+
+function mapToDefaults(map) {
+	var defaults = {};
+
+	for(const [key, value] of map) {
+		defaults[key] = [];
+	}
+
+	return defaults;
+}
+
+class Collection {
+	constructor(name) {
+		this.name = name;
+	}
+
+	async initialize(collection) {
+		this.collection = collection;
+	}
+}
+
+module.exports = class Database {
+	constructor() {
+		this.collections = new Map();
+	}
+
+	async load() {
+		this.database = await loadDatabase(Path.resolve(__dirname, "../../../data/panel.json"));
+
+		await this.database.defaults(mapToDefaults(this.collections)).write();
+
+		for(const [name, collection] of this.collections) {
+			await collection.initialize(this.database.get(name));
+		}
+	}
+
+	create(name) {
+		this.collections.set(name, new Collection(name));
+	}
+
+	has(name) {
+		return this.collections.has(name);
+	}
+
+	get(name) {
+		if(!this.has(name)) return undefined;
+
+		return this.collections.get(name).collection;
+	}
+}
