48e50168e91f790d8cddccdbe2d131e0a6141d09
JortvD <jortvandriel@hotmail.com>
2018-09-23 15:09:02 +0200

    Renamed ConfigFile to AbstractConfigFile

--- src/config/global.js
+++ src/config/global.js
@@ -1,7 +1,7 @@
 const Path = require("path");
 const AbstractConfigFile = require("./abstract");
 
-module.exports = class GlobalConfigManager extends ConfigManager {
+module.exports = class GlobalConfigFile extends AbstractConfigFile {
 	constructor() {
 		let defaultGlobalConfigPath = Path.resolve(__dirname, "../../res/defaults/globalConfig.json");
 		let destGlobalConfigPath = Path.resolve(__dirname, "../../../panel.json");
