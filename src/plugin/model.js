4f92b27b1f4025ec06e9b0b8111c495c0afb3bf9
JortvD <jortvandriel@hotmail.com>
2018-10-13 16:22:37 +0200

    The plugin can now get the best environment index file

--- src/plugin/model.js
+++ src/plugin/model.js
@@ -1,3 +1,15 @@
+function bestEnvironment(enviroments) {
+	let panelPlatform = app.utils.platform.get();
+
+	enviroments.sort(app.utils.array.sortByObjectKey("platform"));
+
+	for(let enviroment of enviroments) {
+		if(app.utils.platform.match(enviroment.platform, panelPlatform)) {
+			return enviroment;
+		}
+	}
+}
+
 class Plugin {
 	constructor(object) {
 		this.name = object.name;
@@ -7,6 +19,16 @@ class Plugin {
 		this.license = object.license;
 		this.panel = object.panel;
 		this.machine = object.machine;
+	}
+
+	initialize() {
+		let enviroment = bestEnvironment(this.panel);
+
+		if(enviroment === undefined) {
+			return console.error(`The plugin ${plugin.name} has no matching environments for the panel`);
+		}
+
+		console.log(enviroment);
 	}
 }
 
