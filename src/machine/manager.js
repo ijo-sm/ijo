05e8f6817f8b88abd4a26cf6d5d2b046600f8191
JortvD <jortvandriel@hotmail.com>
2018-10-19 21:54:26 +0200

    Add create and get functions for the machine manager

--- src/machine/manager.js
+++ src/machine/manager.js
@@ -5,11 +5,26 @@ module.exports = class MachineManager {
 		this.connected = [];
 	}
 
+	async create(secret) {
+		let id = Utils.generate.shortid();
+		let hashedSecret = Utils.crypto.hash(secret);
+
+		await app.db.get("machines").push({
+			id, secret: hashedSecret
+		}).write();
+
+		return this.get("id", id);
+	}
+
 	initialize() {
 		app.db.create("machines");
 	}
 
 	handle(socket) {
 		this.connected.push(new Machine(socket));
+	}
+
+	get(key, value) {
+		return app.db.get("machines").find(machine => machine[key] === value).value();
 	}
 }
