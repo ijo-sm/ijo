088eb6fb2f530d780be308e094ea4d2d5e5c790a
JortvD <jortvandriel@hotmail.com>
2018-10-19 17:24:05 +0200

    Let machine manager add connected machines

--- src/machine/manager.js
+++ src/machine/manager.js
@@ -1,5 +1,15 @@
+const Machine = require("./model");
+
 module.exports = class MachineManager {
+	constructor() {
+		this.connected = [];
+	}
+
 	initialize() {
 		app.db.create("machines");
+	}
+
+	handleMachine(socket) {
+		this.connected.push(new Machine(socket));
 	}
 }
