10eff43c81bac664021c5c9e68fd09e545fb64ca
JortvD <jortvandriel@hotmail.com>
2018-10-19 21:53:19 +0200

    Add a class with machine packets

---
+++ src/net/machine/packets/machine.js
@@ -0,0 +1,32 @@
+const assert = require("assert");
+const PacketList = require("./model");
+const Packet = require("./../packet");
+
+module.exports = class MachinePacketList extends PacketList {
+	init(handler) {
+		super.init(handler);
+
+		handler.addPacket(new Packet("machine/create", "authenticating", this.create));
+		handler.addPacket(new Packet("machine/auth", "authenticating", this.auth));
+	}
+
+	async create(packet, machine) {
+		let secret = Utils.crypto.generateSecret();
+
+		machine.load(await app.machines.create(secret));
+		machine.send("machine/created", {id: machine.id, secret: secret});
+	}
+
+	auth(packet, machine) {
+		assert(packet.id, "The value id of the received packet is undefined");
+		assert(packet.secret, "The value secret of the received packet is undefined");
+
+		machine.load(app.machines.get("id", packet.id));
+
+		if(!machine.checkSecret(packet.secret)) {
+			machine.disconnect("The id and secret didn't match");
+		}
+
+		machine.send("machine/authed");
+	}
+}
