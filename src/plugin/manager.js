3c489e2972debb5fb91658e1ead770f7a81de6ab
JortvD <jortvandriel@hotmail.com>
2018-11-04 11:35:43 +0100

    Move plugin function to the model class

--- src/plugin/manager.js
+++ src/plugin/manager.js
@@ -1,51 +1,7 @@
 const FileSystem = require("fs");
-const pify = require("pify");
-const assert = require("assert");
-const {Plugin, PluginEnvironment} = require("./model");
+const NodeUtils = require("util");
+const Plugin = require("./model");
 
-function loadPlugin(path) {
-	let plugin = require(ijo.utils.path.resolve("plugins/" + path + "/plugin.json"));
-
-	plugin.panel = generateEnvironments(plugin.panel);
-	plugin.machine = generateEnvironments(plugin.machine);
-
-	validatePlugin(plugin);
-
-	return new Plugin(plugin);
-}
-
-function generateEnvironments(environment) {
-	if(environment === undefined) {
-		return [];
-	}
-	else if(environment instanceof Array) {
-		environment.map(function(environment) {
-			validateEnvironment(environment);
-
-			return new PluginEnvironment(environment);
-		});
-		
-		return environment;
-	}
-
-	validateEnvironment(environment);
-
-	return [new PluginEnvironment(environment)];
-}
-
-function validateEnvironment(environment) {
-	assert.equal(typeof environment, "object", "The environment is not an object");
-	assert.equal(typeof environment.lang, "string", "There is no language for the environment");
-	assert.equal(typeof environment.index, "string", "There is no index file for the environment");
-}
-
-function validatePlugin(plugin) {
-	assert.equal(typeof plugin, "object", "The config is not an object");
-	assert.equal(typeof plugin.name, "string", "There is no name in the config");
-	assert.equal(typeof plugin.version, "string", "There is no version in the config");
-
-	assert(plugin.panel instanceof Array, "The config doesn't contain any environments for the panel");
-}
 
 module.exports = class PluginManager {
 	constructor() {
@@ -56,10 +12,10 @@ module.exports = class PluginManager {
 		let paths = await NodeUtils.promisify(FileSystem.readdir)(ijo.utils.path.resolve("plugins/"));
 
 		paths.forEach(function(path) {
-			let plugin;
+			let plugin 
 
 			try {
-				plugin = loadPlugin(path);
+				plugin = new Plugin(path);
 			} catch(e) {
 				return console.error(`The plugin at /panel/plugins/${path} could not be loaded: ${e.message}`);
 			}
--- src/plugin/model.js
+++ src/plugin/model.js
@@ -1,3 +1,6 @@
+
+const Assert = require("assert");
+
 function bestEnvironment(enviroments) {
 	let panelPlatform = ijo.utils.platform.get();
 
@@ -12,15 +15,58 @@ function bestEnvironment(enviroments) {
 	}
 }
 
-class Plugin {
+function parseEnvironments(environment) {
+	if(environment === undefined) {
+		return [];
+	}
+	else if(environment instanceof Array) {
+		environment.map(function(environment) {
+			return new PluginEnvironment(environment);
+		});
+		
+		return environment;
+	}
+
+	return [new PluginEnvironment(environment)];
+}
+
+class PluginEnvironment {
 	constructor(object) {
+		this.validate(object);
+
+		this.platform = object.platform;
+		this.language = object.lang;
+		this.indexFile = object.index;
+		this.includes = object.includes;
+		this.excludes = object.excludes;
+	}
+
+	validate(data) {
+		Assert.equal(typeof data, "object", "The environment is not an object");
+		Assert.equal(typeof data.lang, "string", "There is no language for the environment");
+		Assert.equal(typeof data.index, "string", "There is no index file for the environment");
+	}
+}
+
+module.exports = class Plugin {
+	constructor(path) {
+		let object = require(ijo.utils.path.resolve(`plugins/${path}/plugin.json`));
+
+		this.validate(object);
+
 		this.name = object.name;
 		this.description = object.description;
 		this.version = object.version;
 		this.author = object.author;
 		this.license = object.license;
-		this.panel = object.panel;
-		this.machine = object.machine;
+		this.panel = parseEnvironments(object.panel);
+		this.machine = parseEnvironments(object.machine);
+	}
+
+	validate(data) {
+		Assert.equal(typeof data, "object", "The config is not an object");
+		Assert.equal(typeof data.name, "string", "There is no name in the config");
+		Assert.equal(typeof data.version, "string", "There is no version in the config");
 	}
 
 	initialize() {
@@ -29,17 +75,7 @@ class Plugin {
 		if(enviroment === undefined) {
 			return console.error(`The plugin ${plugin.name} has no matching environments for the panel`);
 		}
+
+		let indexFile = require(ijo.utils.path.resolve(`plugins/${path}/${enviroment.indexFile}`));
 	}
 }
-
-class PluginEnvironment {
-	constructor(object) {
-		this.platform = object.platform;
-		this.language = object.lang;
-		this.indexFile = object.index;
-		this.includes = object.includes;
-		this.excludes = object.excludes;
-	}
-}
-
-module.exports = {Plugin, PluginEnvironment};
