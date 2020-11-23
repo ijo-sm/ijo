a703f8c95ce5ace0fe365d76e21d97e0cbb98a1b
JortvD <10116429+JortvD@users.noreply.github.com>
2020-11-23 16:13:38 +0100

    Add path handling to the api

--- src/net/api.js
+++ src/net/api.js
@@ -1,7 +1,26 @@
 const http = require("http");
 const util = require("util");
+const {URL} = require("url");
 
 class Api {
+	constructor() {
+		this.stack = [];
+	}
+
+	register(path, method = "*", callback) {
+		this.stack.push({
+			path, method, callback
+		});
+	}
+
+	unregister(path) {
+		const handlerIndex = this.stack.findIndex(handler => handler.path === path);
+
+		if(handlerIndex < 0) return;
+
+		this.stack.splice(handlerIndex);
+	}
+
 	initialize() {
 		this.server = http.createServer((req, res) => {
 			this.handle(req, res).catch(err => this.handleError(err))
@@ -10,7 +29,42 @@ class Api {
 	}
 
 	async handle(req, res) {
+		const url = new URL(req.url, "http://localhost/");
 
+		for(let handler of this.stack) {
+			if(handler.method !== "*" && handler.method !== req.method) continue;
+
+			const pathData = this.parsePath(handler.path, url.pathname);
+
+			if(pathData === undefined) continue;
+
+			handler.callback(req, res, pathData);
+		}
+
+		res.statusCode = 404;
+		res.end();
+	}
+
+	parsePath(template, path) {
+		const templateParts = template.split("/");
+		const pathParts = path.split("/");
+		const data = {};
+
+		for(let i = 0; i < templateParts.length; i++) {
+			const templatePart = templateParts[i];
+			const pathPart = pathParts[i];
+
+			if(templatePart.startsWith(":")) {
+				if(pathPart === undefined) return;
+				const key = templatePart.substring(1, templatePart.length);
+
+				data[key] = pathPart;
+			}
+			else if(templatePart === "*") return data;
+			else if(templatePart !== pathPart) return;
+		}
+
+		return data;
 	}
 
 	handleError(err) {
