340246de86e989fea238afdc095f127f9d81b5ec
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2020-12-06 01:41:04 +0100

    Add basic user create api

--- src/user/userApi.js
+++ src/user/userApi.js
@@ -4,16 +4,21 @@ class UserApi extends ApiModel {
     constructor(api, users) {
         super(api);
 
-        api.register("/user/create", "POST", (...args) => this.create(...args));
+        api.register("/user/create", "POST", (...args) => this.create(users, ...args));
     }
 
-    start({database}) {
-        super.start();
-
-        this.collection = database.collection("users");
-    }
-
-    create(req, res) {
+    /**
+     * Creates a new user.
+     * @param {Users} users The users class.
+     * @param {ApiRequest} req The api request.
+     * @param {ApiResponse} res The api response.
+     */
+    async create(users, req, res) {
+        const data = await req.bodyAsJSON();
+        // TODO: Check if username has already been used.
+        // TODO: Check if data is correct (eg username and password are not missing).
+        const user = users.create({username: data.username, password: data.password});
+        await users.collection.addOne(user).catch(e => res.sendError({message: e.message, code: 500}));
 
         res.send({data: {message: "Created"}, code: 201});
     }
