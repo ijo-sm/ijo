78ea75a909656489e5b3edb1f8bea6f8917f4058
The Otterlord <64310361+TheOtterlord@users.noreply.github.com>
2020-12-17 15:54:13 +0000

    Add validation to user creation

--- src/user/api.js
+++ src/user/api.js
@@ -15,8 +15,21 @@ class UserApi extends ApiModel {
      */
     async create(users, req, res) {
         const data = await req.bodyAsJSON();
+        
+        // Validate incoming data
+        if (!data?.username) {res.sendError({message: "Missing required data 'username'", code: 500});return}
+        if (!data?.password) {res.sendError({message: "Missing required data 'password'", code: 500});return}
+        if (typeof(data?.username) != "string") {
+            res.sendError({message: "Invalid Data: 'username' was invalid type", code: 500});
+            return;
+        }
+        if (typeof(data.password) != "string") {
+            res.sendError({message: "Invalid Data: 'password' was invalid type", code: 500});
+            return;
+        }
+
         // TODO: Check if username has already been used.
-        // TODO: Check if data is correct (eg username and password are not missing).
+
         const user = users.create({username: data.username, password: data.password});
         await users.collection.addOne(user).catch(e => res.sendError({message: e.message, code: 500}));
 
